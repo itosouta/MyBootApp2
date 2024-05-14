c8c4444ba04007357013db2f1b201c4918ccc881
伊藤 颯太 <210832@jc-21.jp>
2024-05-14 11:19:04 +0900

    課題2

--- src/main/java/jp/te4a/spring/boot/myapp1/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp1/HelloController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController
 {
-@RequestMapping("/")
-public String index() {
-return "Hello, Spring Boot!";
+@RequestMapping("/taro")
+public String taro() {
+return "Taro desu!";
 }
 }
